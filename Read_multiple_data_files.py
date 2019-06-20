9a2d2164800e0ce8fc58c0d107b98292c8cd856e
gotamyers <gotardo.fernando@gmail.com>
2019-06-20 16:28:55 +1000

    Working for now...

--- Read_multiple_data_files.py
+++ Read_multiple_data_files.py
@@ -16,11 +16,15 @@ for k in range(11):
                 #print(row)
                 df_temp.append(row)
             df = df_temp[31:]
-            data['SSA_' + str(k+1) + '_exp_' + str(i+1)] = np.array(df)
+            data['SSA_' + str(k+1) + '_exp_' + str(i+1)] = np.array(df, dtype=object)
 data['SSA_2_exp_1'] = data['SSA_2_exp_1'][:,0:2]
 data['SSA_8_exp_1'] = data['SSA_8_exp_1'][:,0:2]
-#print(data)
 
-
-
+sum = []
+for row in range(751):
+    c = float(data['SSA_1_exp_1'][row,1]) + float(data['SSA_3_exp_1'][row,1])
+    sum = np.insert(sum,row, c)
+sum = sum.reshape(751,1)
+#     print(c)
+#print(sum)
 final = time.time()
