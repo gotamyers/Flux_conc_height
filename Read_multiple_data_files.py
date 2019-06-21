98f21c8ee253b44cca69bd5a19fa22eeaa167371
gotamyers <gotardo.fernando@gmail.com>
2019-06-21 15:03:43 +1000

    Working for now...

--- Read_multiple_data_files.py
+++ Read_multiple_data_files.py
@@ -1,10 +1,12 @@
 import csv
 import time
 import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.ticker as mticker
 
 initial = time.time()
 
-B = 5e-3
+B = 5e-4
 RBW = 300
 
 data = {}
@@ -64,19 +66,39 @@ for k in range(13):
 
         data['TRACE' + str(k + 1)] = np.reshape(np.array(df), (-1, 2))
 
-Bmin = []
 
-for k in range(13):
+S21_Snn_ref_ratio = np.zeros(11)
+Bmin_min = np.zeros(11)
+for k in range(11):
+    Bmin = []
+    S21_Snn_ref_ratio[k] = data['TRACE' + str(k + 1)][8, 1]/data['TRACE13'][8, 1]
 
     for row in range(751):
-        c = float(data['TRACE' + str(k + 1)][row, 1]) - float(data['TRACE13'][row, 1])
-        #c = float(data['SSA_' + str(k + 1) + '_exp_2'][row, 1]) - mean
-        SNR.append(c)
-    data['SNR' + str(k + 1)] = np.array(SNR)
+        c = np.multiply(np.sqrt(np.multiply(np.divide(data['TRACE13'][row,1], data['TRACE' + str(k + 1)][row,1]),S21_Snn_ref_ratio[k])),Bmin_ref[k])
+        Bmin.append(c)
 
-Bmin_ref[k] = np.divide(B, (np.sqrt(data['SNR' + str(k + 1)][370:440].max() * RBW)))
+    for j in range(len(Bmin)):
+        Bmin[j] = np.float(Bmin[j])
 
-#print(data['TRACE1'].shape)
 
+
+    data['Bmin'] = np.reshape(np.array(Bmin), (-1,1))
+
+    Bmin_min[k] = np.divide(data['Bmin'].min(), 1e-6)
+
+
+
+height = [30, 60, 90, 150, 210, 270, 470, 670, 1000, 2000, 2400]
+height = np.array(height)
 
 final = time.time()
+print('\n' + str(final - initial) + ' seconds')
+
+
+
+plt.plot(height, Bmin_min, 'ro')
+plt.xscale('log')
+plt.xlabel(r'$\Delta$z ($\mu$m)')
+plt.ylabel('Minimal sensitivity ($\mu$T$/\sqrt{Hz}$)')
+
+plt.show()
